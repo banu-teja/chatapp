9cb049f289623c63d4b0e58a66990a32c4c8269c
bhanuteja.pappala <bhanuteja.pappala@gmail.com>
2024-11-02 20:32:10 +0000

    random dice rolls

---
+++ lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:myapp/dice_roller.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Colors.purpleAccent, Colors.pinkAccent],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
